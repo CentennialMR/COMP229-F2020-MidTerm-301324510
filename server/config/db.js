223b7387bdbc7a6eb95652c916848a4736d658cb
Mariela Ramos <maricrv@gmail.com>
2023-03-04 12:47:42 -0500

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //local MongoDB deployment ->
+
+  //"URI": "mongodb://127.0.0.1:27017/books229"
+  "URI":"mongodb+srv://mariela:Te8nuEsb6Y3bfj5T@cluster0.pcejwme.mongodb.net/books229?retryWrites=true&w=majority"
+  //"URI": "mongodb+srv://mariela:Te8nuEsb6Y3bfj5T@cluster0.pcejwme.mongodb.net/books229"
+}
+
